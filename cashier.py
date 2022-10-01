c388d517512c5c483c62c393c0351294d457aec3
Talal <talal.alothman@gmail.com>
2022-10-01 20:40:49 +0300

    almost done

--- cashier.py
+++ cashier.py
@@ -11,14 +11,27 @@ def get_invoice_items(items):
     # ['1 Apple 0.200KD', '4 Orange 1.200KD']
     # ---
     # Write your code here
-    ...
+    items = [
+        {"name" : "Apple", "quantity" : 1, "price" : .2},
+        {"name" : "Orange", "quantity" : 4, "price" : .3},
+             ]
 
+    for item in items:
+        invoice_items = [item['quantity'], item['name'], float(item['quantity'] * item['price'])]
+    return invoice_items
+    
+    
+    
 
 def get_total(items):
     # Items is a dictionary with a quantity and price key
     # Calculate the total of all items in the cart
     # Write your code here
-    ...
+    total = 0
+    for item in items:
+        subtotal = [item['quantity'] * item['price']]
+        return total + subtotal
+    return total
 
 
 def print_receipt(invoice_items, total):
@@ -28,12 +41,51 @@ def print_receipt(invoice_items, total):
     # total at the end.
     # ---
     # Write your code here
-    ...
+    print("--------------------------")
+    print("Receipt")
+    print("--------------------------")
+    
+    for item in invoice_items:
+        print(item[get_invoice_items])
+
+    print("--------------------------")
+    print("Total Price: " + float(total) + "KD")
+
 
 
 def main():
     # Write your main logic here
-    ...
+    items = [
+    {
+        "name": "apples",
+        "price": .2,
+        "quantity": 4
+    },
+    {
+        "name": "carrot",
+        "price": .1,
+        "quantity": 1 
+    },
+    {
+        "name": "flour",
+        "price": 1.3,
+        "quantity": 2
+    },
+    {
+        "name": "water bottles",
+        "price": .05,
+        "quantity": 10
+    },
+            ]
+
+    while not "done":
+        items["name"] = input('Item (enter "done" when finished): ')
+        items["price"] = input("Price: ")
+        items["quantity"] = input("Quantity: ")
+    else:
+        print(print_receipt)
+
+
 
 
 if __name__ == "__main__":
